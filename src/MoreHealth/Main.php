33d56d5da3958ca38b1100df5c4d416cbea012b8
iksaku <iksaku@me.com>
2014-06-24 14:17:24 +0800

    Optimized command handling
    Sorry I changed a lot of things but I believe that they are all necessary. Reverse them if you like (but please don't delete and recreate)

--- src/MoreHealth/Main.php
+++ src/MoreHealth/Main.php
@@ -3,6 +3,7 @@ namespace MoreHealth;
 
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
+use pocketmine\command\ConsoleCommandSender;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerLoginEvent;
 use pocketmine\Player;
@@ -11,167 +12,120 @@ use pocketmine\utils\Config;
 use pocketmine\utils\TextFormat;
 
 class Main extends PluginBase implements Listener{
-    public function onEnable(){
-        $this->getServer()->getPluginManager()->registerEvents($this, $this);
-    }
+	public function onEnable(){
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+	}
+	public function onCommand(CommandSender $sender, Command $command, $alias, array $args){
+		switch($command->getName()){
+			case "morehealth":
+				if(!isset($args[0])){
+					return false;
+				}
+				switch($args[0]){
+					case "set":
+						if(!$sender->hasPermission("morehealth.set")){
+							$sender->sendMessage($command->getPermissionMessage());
+							return true;
+						}
+						if(!isset($args[1])){
+							$sender->sendMessage(TextFormat::RED."Usage: /morehealth set <hearts> [player]");
+							return true;
+						}
+						if(($sender instanceof ConsoleCommandSender) and !isset($args[2])){ // RemoteConsoleXxx instanceof ConsoleXxx
+							$sender->sendMessage(TextFormat::RED."Usage: /morehealth set <hearts> <player>");
+						}
+						$hearts = $args[1];
+						$amount = (int) ($hearts * 2);
+						/** @var Player $target */
+						$target = $sender;
+						if(isset($args[2])){
+							$target = $this->getServer()->getPlayer($args[2]);
+							if(!($target instanceof Player)){
+								$sender->sendMessage(TextFormat::YELLOW."No players with a name similar to $args[2] are found.");
+								return true;
+							}
+						}
+						$this->setMaxHealth($target, $amount, true);
+						return true;
+					case "restore":
+						if(!isset($args[1]) and ($sender instanceof ConsoleCommandSender)){
+							$sender->sendMessage(TextFormat::RED."Usage: /morehealth restore <player>");
+							return true;
+						}
+						/** @var Player $target */
+						$target = $sender;
+						if(isset($args[1])){
+							$target = $this->getServer()->getPlayer($args[1]); // this function gets the player object with the name most similar to $args[1]
+							if(!($target instanceof Player)){
+								$sender->sendMessage(TextFormat::YELLOW."No players with a name similar to $args[1] are found.");
+								return true;
+							}
+						}
+						$this->setMaxHealth($target, 20, true);
+						return true;
+					default:
+						return false;
+				}
+			default:
+				return false;
+		}
+	}
 
-    public function onCommand(CommandSender $sender, Command $command, $alias, array $args){
-        switch($command->getName()){
-            case "morehealth":
-                if(count($args) < 1 || count($args) > 3){
-                    $sender->sendMessage(TextFormat::RED . $command->getUsage());
-                }else{
-                    if(!$sender->hasPermission("morehealth.set") || !$sender->hasPermission("morehealth.restore")){
-                        $sender->sendMessage(TextFormat::RED . $command->getPermissionMessage());
-                    }else{
-                        switch(count($args)){
-                            case 1:
-                                if($args[0] == "set"){
-                                    if(!$sender instanceof Player){
-                                        $sender->sendMessage(TextFormat::RED . "Please specify an amount and a player: /morehealth set <amount> <player>");
-                                    }else{
-                                        $sender->sendMessage(TextFormat::RED . "Please specify an amount: /morehealth set <amount>");
-                                    }
-                                }elseif($args[0] == "restore"){
-                                    if(!$sender instanceof Player){
-                                        $sender->sendMessage(TextFormat::RED . "Please specify a player: /morehealth restore <player>");
-                                    }else{
-                                        $this->setMaxHealth($sender, 20, true);
-                                        $sender->sendMessage(TextFormat::AQUA . "Your health has been restored to [20] points");
-                                    }
-                                }else{
-                                    $sender->sendMessage(TextFormat::RED . $command->getUsage());
-                                }
-                                return true;
-                                break;
-                            case 2:
-                                if($args[0] == "set"){
-                                    $amount = $args[1];
-                                    if(!$sender instanceof Player){
-                                        $sender->sendMessage(TextFormat::RED . "Please specify a player: /morehealth set <amount> <player>");
-                                    }else{
-                                        if(!is_numeric($amount)){
-                                            $sender->sendMessage(TextFormat::YELLOW . "Invalid health amount, please use numbers");
-                                        }else{
-                                            $this->setMaxHealth($sender, $amount, true);
-                                            $sender->sendMessage(TextFormat::AQUA . "You now have [$amount] points of health");
-                                        }
-                                    }
-                                }elseif($args[0] == "restore"){
-                                    if(!$sender->hasPermission("morehealth.restore.other")){
-                                        $sender->sendMessage(TextFormat::RED . $command->getPermissionMessage());
-                                    }else{
-                                        $player = $this->getPlayer($args[1]);
-                                        if($player == false){
-                                            $sender->sendMessage(TextFormat::RED . "[Error] Can't find player $args[1]");
-                                        }else{
-                                            $this->setMaxHealth($player, 20, true);
-                                            $player->sendMessage(TextFormat::AQUA . "Your health has been restored to [20] points");
-                                            if($player != $sender){
-                                                if(substr($args[0], -1, 1) == "s"){
-                                                    $sender->sendMessage(TextFormat::AQUA . "$args[1]' health has been restored to [20] points");
-                                                }else{
-                                                    $sender->sendMessage(TextFormat::AQUA . "$args[1]'s health has been restored to [20] points");
-                                                }
-                                            }
-                                        }
-                                    }
-                                }else{
-                                    $sender->sendMessage(TextFormat::RED . $command->getUsage());
-                                }
-                                return true;
-                                break;
-                            case 3:
-                                if($args[0] == "set"){
-                                    $amount = $args[1];
-                                    $player = $this->getPlayer($args[2]);
-                                    if(!$sender->hasPermission("morehealth.set.other")){
-                                        $sender->sendMessage(TextFormat::RED . $command->getPermissionMessage());
-                                    }else{
-                                        if($player == false){
-                                            $sender->sendMessage(TextFormat::RED . "[Error] Can't find player $args[2]");
-                                        }else{
-                                            if(!is_numeric($amount)){
-                                                $sender->sendMessage(TextFormat::YELLOW . "Invalid health amount, please use numbers");
-                                            }else{
-                                                $this->setMaxHealth($player, $amount, true);
-                                                $player->sendMessage(TextFormat::AQUA . "You now have [$amount] points of health");
-                                                if($player != $sender){
-                                                    $sender->sendMessage(TextFormat::AQUA . "$args[2] now have [$amount] points of health");
-                                                }
-                                            }
-                                        }
-                                    }
-                                }elseif($args[0] == "restore"){
-                                    $sender->sendMessage(TextFormat::RED . "Usage: /morehealth restore <player>");
-                                }else{
-                                    $sender->sendMessage(TextFormat::RED . $command->getUsage());
-                                }
-                                return true;
-                                break;
-                        }
-                    }
-                }
-                return true;
-                break;
-        }
-        return true;
-    }
+	/**
+	 * @param PlayerLoginEvent $event
+	 */
+	public function onPlayerLogin(PlayerLoginEvent $event){
+		$player = $event->getPlayer();
+		$this->setMaxHealth($player, $this->getMaxHealth($player));
+	}
 
-    /**
-     * @param PlayerLoginEvent $event
-     */
-    public function onPlayerLogin(PlayerLoginEvent $event){
-        $player = $event->getPlayer();
-        $this->setMaxHealth($player, $this->getMaxHealth($player));
-    }
+	public function getPlayer($name){
+		$r = "";
+		foreach($this->getServer()->getOnlinePlayers() as $p){
+			if($p->getName() == $name || $p->getDisplayName() == $name){
+				$r = $this->getServer()->getPlayerExact($p->getName());
+			}
+		}
+		if($r == ""){
+			return false;
+		}else{
+			return $r;
+		}
+	}
 
-    public function getPlayer($name){
-        $r = "";
-        foreach($this->getServer()->getOnlinePlayers() as $p){
-            if($p->getName() == $name || $p->getDisplayName() == $name){
-                $r = $this->getServer()->getPlayerExact($p->getName());
-            }
-        }
-        if($r == ""){
-            return false;
-        }else{
-            return $r;
-        }
-    }
+	public function setMaxHealth(Player $player, $amount, $save = false){
+		if(!is_numeric($amount)){
+			return false;
+		}else{
+			$player->setMaxHealth($amount);
+			$player->setHealth($player->getMaxHealth());
+			if($save == true){
+//				$this->saveMaxHealth($player, $amount);
+				// I think PocketMine will store it later. Anyway, if you I/O the config every time, it will lag large servers, especially <quote>minigame servers</quote>.
+			}
+			return true;
+		}
+	}
 
-    public function setMaxHealth(Player $player, $amount, $save = false){
-        if(!is_numeric($amount)){
-            return false;
-        }else{
-            $player->setMaxHealth($amount);
-            $player->setHealth($player->getMaxHealth());
-            if($save == true){
-                $this->saveMaxHealth($player, $amount);
-            }
-            return true;
-        }
-    }
+	public function saveMaxHealth(Player $player, $amount){
+		$config = new Config("plugins/MoreHealth.yml", Config::YAML);
+		if(!is_numeric($amount)){
+			return false;
+		}else{
+			$config->set($player->getName(), $amount);
+			$config->save();
+			return true;
+		}
+	}
 
-    public function saveMaxHealth(Player $player, $amount){
-        $config = new Config("plugins/MoreHealth.yml", Config::YAML);
-        if(!is_numeric($amount)){
-            return false;
-        }else{
-            $config->set($player->getName(), $amount);
-            $config->save();
-            return true;
-        }
-    }
-
-    public function getMaxHealth(Player $player){
-        $config = new Config("plugins/MoreHealth.yml", Config::YAML);
-        if(!$config->exists($player->getName())){
-            return false;
-        }elseif(is_numeric($config->get($player->getName()))){
-            return $config->get($player->getName());
-        }else{
-            return false;
-        }
-    }
+	public function getMaxHealth(Player $player){
+		$config = new Config("plugins/MoreHealth.yml", Config::YAML);
+		if(!$config->exists($player->getName())){
+			return 20;
+		}elseif(is_numeric($config->get($player->getName()))){
+			return $config->get($player->getName());
+		}else{
+			return false;
+		}
+	}
 }
