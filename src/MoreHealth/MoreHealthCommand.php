9d049d76488540d63f96685f86d548eceb9836a4
iksaku <iksaku@me.com>
2014-07-16 14:08:22 -0500

    Fixed permission nodes

--- src/MoreHealth/MoreHealthCommand.php
+++ src/MoreHealth/MoreHealthCommand.php
@@ -22,132 +22,163 @@ class MoreHealthCommand extends Command implements PluginIdentifiableCommand{
         }
         if(count($args) < 1 || count($args) > 3){
             $sender->sendMessage(TextFormat::RED . $this->getUsage());
-        }else{
-            if(!$sender->hasPermission("morehealth.set") || !$sender->hasPermission("morehealth.restore")){
-                $sender->sendMessage(TextFormat::RED . $this->getPermissionMessage());
-            }else{
-                switch(count($args)){
-                    case 1:
-                        switch($args[0]){
-                            case "setdefault":
-                                $sender->sendMessage(TextFormat::RED . "Please specify an amount: /morehealth setdefault <amount>");
-                                return true;
-                                break;
-                            case "set":
-                                if(!$sender instanceof Player){
-                                    $sender->sendMessage(TextFormat::RED . "Please specify an amount and a player: /morehealth set <amount> <player>");
-                                }else{
-                                    $sender->sendMessage(TextFormat::RED . "Please specify an amount: /morehealth set <amount>");
-                                }
-                                return true;
-                                break;
-                            case "restore":
-                                if(!$sender instanceof Player){
-                                    $sender->sendMessage(TextFormat::RED . "Please specify a player: /morehealth restore <player>");
-                                }else{
-                                    $this->plugin->setMaxHealth($sender, 20, true);
-                                    $sender->sendMessage(TextFormat::AQUA . "Your health has been restored to [20] points");
-                                }
-                                return true;
-                                break;
-                            default:
-                                $sender->sendMessage(TextFormat::RED . $this->getUsage());
-                                break;
+            return false;
+        }
+        if(!$sender->hasPermission("morehealth.set") || !$sender->hasPermission("morehealth.restore")){
+            $sender->sendMessage(TextFormat::RED . $this->getPermissionMessage());
+            return false;
+        }
+        switch(count($args)){
+            case 1:
+                switch($args[0]){
+                    case "setdefault":
+                        if(!$sender->hasPermission("morehealth.setdefault")){
+                            $sender->sendMessage(TextFormat::RED . $this->getPermissionMessage());
+                            return false;
+                        }
+                        $sender->sendMessage(TextFormat::RED . "Please specify an amount: /morehealth setdefault <amount>");
+                        return true;
+                        break;
+                    case "set":
+                        if(!$sender->hasPermission("morehealth.set.use")){
+                            $sender->sendMessage(TextFormat::RED . $this->getPermissionMessage());
+                            return false;
+                        }
+                        if(!$sender instanceof Player){
+                            $sender->sendMessage(TextFormat::RED . "Please specify an amount and a player: /morehealth set <amount> <player>");
+                        }else{
+                            $sender->sendMessage(TextFormat::RED . "Please specify an amount: /morehealth set <amount>");
                         }
                         return true;
                         break;
-                    case 2:
-                        switch($args[0]){
-                            case "setdefault":
-                                $amount = $args[1];
-                                if(!is_numeric($amount)){
-                                    $sender->sendMessage(TextFormat::YELLOW . "Invalid health amount, please use numbers");
-                                }else{
-                                    $this->plugin->setDefaultHealth($amount);
-                                    $sender->sendMessage(TextFormat::AQUA . "Successfully changed the default health limit.");
-                                }
-                                return true;
-                                break;
-                            case "set":
-                                if(!$sender instanceof Player){
-                                    $sender->sendMessage(TextFormat::RED . "Please specify a player: /morehealth set <amount> <player>");
-                                }else{
-                                    $amount = $args[1];
-                                    if(!is_numeric($amount)){
-                                        $sender->sendMessage(TextFormat::YELLOW . "Invalid health amount, please use numbers");
-                                    }else{
-                                        $this->plugin->setMaxHealth($sender, $amount, true);
-                                        $sender->sendMessage(TextFormat::AQUA . "You now have [$amount] points of health");
-                                    }
-                                }
-                                return true;
-                                break;
-                            case "restore":
-                                if(!$sender->hasPermission("morehealth.restore.other")){
-                                    $sender->sendMessage(TextFormat::RED . $this->getPermissionMessage());
-                                }else{
-                                    $player = $this->plugin->getPlayer($args[1]);
-                                    if($player == false){
-                                        $sender->sendMessage(TextFormat::RED . "[Error] Can't find player $args[1]");
-                                    }else{
-                                        $this->plugin->setMaxHealth($player, 20, true);
-                                        $player->sendMessage(TextFormat::AQUA . "Your health has been restored to [20] points");
-                                        if($player != $sender){
-                                            if(substr($args[0], -1, 1) == "s"){
-                                                $sender->sendMessage(TextFormat::AQUA . "$args[1]' health has been restored to [20] points");
-                                            }else{
-                                                $sender->sendMessage(TextFormat::AQUA . "$args[1]'s health has been restored to [20] points");
-                                            }
-                                        }
-                                    }
-                                }
-                                return true;
-                                break;
-                            default:
-                                $sender->sendMessage(TextFormat::RED . $this->getUsage());
-                                break;
+                    case "restore":
+                        if(!$sender->hasPermission("morehealth.restore.use")){
+                            $sender->sendMessage(TextFormat::RED . $this->getPermissionMessage());
+                            return false;
+                        }
+                        if(!$sender instanceof Player){
+                            $sender->sendMessage(TextFormat::RED . "Please specify a player: /morehealth restore <player>");
+                        }else{
+                            $this->plugin->setMaxHealth($sender, 20, true);
+                            $sender->sendMessage(TextFormat::AQUA . "Your health has been restored to [20] points");
                         }
                         return true;
                         break;
-                    case 3:
-                        switch($args[0]){
-                            case "setdefault":
-                                $sender->sendMessage(TextFormat::RED . "Usage: /morehealth setdefault <amount>");
-                                return true;
-                            case "set":
-                                if(!$sender->hasPermission("morehealth.set.other")){
-                                    $sender->sendMessage(TextFormat::RED . $this->getPermissionMessage());
-                                }else{
-                                    $amount = $args[1];
-                                    $player = $this->plugin->getPlayer($args[2]);
-                                    if($player == false){
-                                        $sender->sendMessage(TextFormat::RED . "[Error] Can't find player $args[2]");
-                                    }else{
-                                        if(!is_numeric($amount)){
-                                            $sender->sendMessage(TextFormat::YELLOW . "Invalid health amount, please use numbers");
-                                        }else{
-                                            $this->plugin->setMaxHealth($player, $amount, true);
-                                            $player->sendMessage(TextFormat::AQUA . "You now have [$amount] points of health");
-                                            if($player != $sender){
-                                                $sender->sendMessage(TextFormat::AQUA . "$args[2] now have [$amount] points of health");
-                                            }
-                                        }
-                                    }
-                                }
-                                return true;
-                                break;
-                            case "restore":
-                                $sender->sendMessage(TextFormat::RED . "Usage: /morehealth restore <player>");
-                                return true;
-                                break;
-                            default:
-                                $sender->sendMessage(TextFormat::RED . $this->getUsage());
-                                break;
-                        }
-                        return true;
+                    default:
+                        $sender->sendMessage(TextFormat::RED . $this->getUsage());
                         break;
                 }
-            }
+                return true;
+                break;
+            case 2:
+                switch($args[0]){
+                    case "setdefault":
+                        if(!$sender->hasPermission("morehealth.setdefault")){
+                            $sender->sendMessage(TextFormat::RED . $this->getPermissionMessage());
+                            return false;
+                        }
+                        $amount = $args[1];
+                        if(!is_numeric($amount)){
+                            $sender->sendMessage(TextFormat::YELLOW . "Invalid health amount, please use numbers");
+                        }else{
+                            $this->plugin->setDefaultHealth($amount);
+                            $sender->sendMessage(TextFormat::AQUA . "Successfully changed the default health limit.");
+                        }
+                        return true;
+                        break;
+                    case "set":
+                        if(!$sender->hasPermission("morehealth.set.use")){
+                            $sender->sendMessage(TextFormat::RED . $this->getPermissionMessage());
+                            return false;
+                        }
+                        if(!$sender instanceof Player){
+                            $sender->sendMessage(TextFormat::RED . "Please specify a player: /morehealth set <amount> <player>");
+                        }else{
+                            $amount = $args[1];
+                            if(!is_numeric($amount)){
+                                $sender->sendMessage(TextFormat::YELLOW . "Invalid health amount, please use numbers");
+                            }else{
+                                $this->plugin->setMaxHealth($sender, $amount, true);
+                                $sender->sendMessage(TextFormat::AQUA . "You now have [$amount] points of health");
+                            }
+                        }
+                        return true;
+                        break;
+                    case "restore":
+                        if(!$sender->hasPermission("morehealth.restore.use")){
+                            $sender->sendMessage(TextFormat::RED . $this->getPermissionMessage());
+                            return false;
+                        }
+                        if(!$sender->hasPermission("morehealth.restore.other")){
+                            $sender->sendMessage(TextFormat::RED . $this->getPermissionMessage());
+                        }else{
+                            $player = $this->plugin->getPlayer($args[1]);
+                            if($player == false){
+                                $sender->sendMessage(TextFormat::RED . "[Error] Can't find player $args[1]");
+                            }else{
+                                $this->plugin->setMaxHealth($player, 20, true);
+                                $player->sendMessage(TextFormat::AQUA . "Your health has been restored to [20] points");
+                                if($player != $sender){
+                                    if(substr($args[0], -1, 1) == "s"){
+                                        $sender->sendMessage(TextFormat::AQUA . "$args[1]' health has been restored to [20] points");
+                                    }else{
+                                        $sender->sendMessage(TextFormat::AQUA . "$args[1]'s health has been restored to [20] points");
+                                    }
+                                }
+                            }
+                        }
+                        return true;
+                        break;
+                    default:
+                        $sender->sendMessage(TextFormat::RED . $this->getUsage());
+                        break;
+                }
+                return true;
+                break;
+            case 3:
+                switch($args[0]){
+                    case "setdefault":
+                        if(!$sender->hasPermission("morehealth.setdefault")){
+                            $sender->sendMessage(TextFormat::RED . $this->getPermissionMessage());
+                            return false;
+                        }
+                        $sender->sendMessage(TextFormat::RED . "Usage: /morehealth setdefault <amount>");
+                        return true;
+                    case "set":
+                        if(!$sender->hasPermission("morehealth.set.other")){
+                            $sender->sendMessage(TextFormat::RED . $this->getPermissionMessage());
+                        }
+                        $amount = $args[1];
+                        $player = $this->plugin->getPlayer($args[2]);
+                        if($player == false){
+                            $sender->sendMessage(TextFormat::RED . "[Error] Can't find player $args[2]");
+                        }else{
+                            if(!is_numeric($amount)){
+                                $sender->sendMessage(TextFormat::YELLOW . "Invalid health amount, please use numbers");
+                            }else{
+                                $this->plugin->setMaxHealth($player, $amount, true);
+                                $player->sendMessage(TextFormat::AQUA . "You now have [$amount] points of health");
+                                if($player != $sender){
+                                    $sender->sendMessage(TextFormat::AQUA . "$args[2] now have [$amount] points of health");
+                                }
+                            }
+                        }
+                        return true;
+                        break;
+                    case "restore":
+                        if(!$sender->hasPermission("morehealth.restore.other")){
+                            $sender->sendMessage(TextFormat::RED . $this->getPermissionMessage());
+                            return false;
+                        }
+                        $sender->sendMessage(TextFormat::RED . "Usage: /morehealth restore <player>");
+                        return true;
+                        break;
+                    default:
+                        $sender->sendMessage(TextFormat::RED . $this->getUsage());
+                        break;
+                }
+                return true;
+                break;
         }
         return true;
     }
